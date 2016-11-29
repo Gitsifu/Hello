5e62a5667c951761fef30e5906a94393e0df4bee
sifu <2587107273@qq.com>
2016-11-29 15:26:41 +0800

    我就是要提交

---
+++ src/com/jxutbst/sifu/Main.java
@@ -0,0 +1,10 @@
+package com.jxutbst.sifu;
+
+/**
+ * Created by sifu on 16-11-29.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
